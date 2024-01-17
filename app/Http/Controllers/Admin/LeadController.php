cbd8217f37570cce97601226beff3e3c4bec345b
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-17 17:58:21 +0100

    Add fillable in model Add store function in lead controller to save datas and send mail

--- app/Http/Controllers/Admin/LeadController.php
+++ app/Http/Controllers/Admin/LeadController.php
@@ -2,13 +2,36 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Mail\NewContact;
+use App\Models\Lead;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
     public function create()
     {
         return view("admin.leads.create");
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name"=> "required|max:255",
+            "email"=> "required|max:255|email",
+            "message"=> "required|min:18",
+            "privacy_policy"=> "required",
+        ]);
+
+        $data = $request->all();
+
+        $lead = Lead::create($data);
+
+        $mail = new NewContact($lead);
+
+        Mail::to('fake@mail.com')->send($mail);
+
+        return back();
+
     }
 }
--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Lead extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'message', 'privacy_policy'
+    ] ;
 }
